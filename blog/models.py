9169b111f754f0ea7070141ddebd922b16c39dc8
Agnieszka Bialek <agnieszkabialekmusic@gmail.com>
2024-07-12 13:18:14 +0000

    Fix the error with models.BooleanField

--- blog/models.py
+++ blog/models.py
@@ -25,6 +25,6 @@ class Comment(models.Model):
         User, on_delete=models.CASCADE, related_name="commenter"
     )
     body = models.TextField()
-    approved = BooleanField(default=False)
+    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
